2cc83d03afa024cfe5f274f106b2c70105125676
derHerrMannelig <derherrmannelig@gmail.com>
2023-05-07 17:22:47 +0300

    feat: add solution for 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+const source = path.join('./04-copy-directory', '/files')
+const folder = path.join('./04-copy-directory', '/files-copy')
 
+async function deleteFolder() {
+  try {
+    const stats = await fs.stat(folder);
+    if (stats.isDirectory()) {
+      console.log('Folder already exists. Rewriting...');
+      await fs.rm(folder, { recursive: true });
+    }
+  } catch (err) {
+    console.log('Folder does not exist. Creating new folder...');
+  }
+}
+
+async function copyFolder() {
+  try {
+    const createDir = await fs.mkdir(folder, { recursive: true });
+    console.log(`Done: ${createDir}`);
+    const contents = await fs.readdir(source);
+    for (const file of contents) {
+      await fs.copyFile(path.join(source, file), path.join(folder, file));
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function index() {
+  try {
+    await deleteFolder();
+    await copyFolder();
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+index();
